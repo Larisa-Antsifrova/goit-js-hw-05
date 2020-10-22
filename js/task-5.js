3e111b6ffb03d35e8f6bd94de4be6bb170e2e260
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-10-22 13:05:49 +0300

    Added solution to the 5th task

--- js/task-5.js
+++ js/task-5.js
@@ -2,20 +2,20 @@ console.log(
   "Task 5 ------------------------------------------------------------"
 );
 class Car {
-  // Write code under this line
+  // Unscalable variant, but with 'price'
   static getSpecs(car) {
     return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
   }
+  // // Scalable variant, but with '_price'
+  // static getSpecs(car) {
+  //   let keys = Object.keys(car);
+  //   let specs = "";
 
-  static getSpecs(car) {
-    let keys = Object.keys(car);
-    let specs = "";
-
-    for (const key of keys) {
-      specs += `${key}: ${car[key]}, `;
-    }
-    return specs.slice(0, -2);
-  }
+  //   for (const key of keys) {
+  //     specs += `${key}: ${car[key]}, `;
+  //   }
+  //   return specs.slice(0, -2);
+  // }
 
   constructor(car, speed = 0, isOn = false, distance = 0) {
     this.maxSpeed = car.maxSpeed;
@@ -52,6 +52,7 @@ class Car {
     if (this.speed - value >= 0) {
       return (this.speed = this.speed - value);
     }
+    return (this.speed = 0);
   }
   drive(hours) {
     if (this.isOn) {
@@ -61,31 +62,47 @@ class Car {
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-// console.log(mustang);
+console.log(mustang);
+// Car {maxSpeed: 200, speed: 0, isOn: false, distance: 0, _price: 2000}
 
 console.log(Car.getSpecs(mustang));
-// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+//"maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000"
 
-// console.log(mustang.turnOn());
-// console.log(Car.getSpecs(mustang));
+console.log(mustang.turnOn());
+//true
 
-// console.log(mustang.accelerate(50));
-// console.log(Car.getSpecs(mustang));
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 0, isOn: true, distance: 0, price: 2000"
 
-// console.log(mustang.drive(2));
-// console.log(Car.getSpecs(mustang));
+console.log(mustang.accelerate(50));
+//50
 
-// mustang.decelerate(20);
-// console.log(Car.getSpecs(mustang));
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 50, isOn: true, distance: 0, price: 2000"
 
-// mustang.drive(1);
-// console.log(Car.getSpecs(mustang));
-// mustang.turnOff();
-// console.log(Car.getSpecs(mustang));
+console.log(mustang.drive(2));
+//100
 
-// console.log(Car.getSpecs(mustang));
-// // // 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000"
 
-// console.log(mustang.price); // 2000
-// mustang.price = 4000;
-// console.log(mustang.price); // 4000
+mustang.decelerate(20);
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 30, isOn: true, distance: 100, price: 2000"
+mustang.drive(1);
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 30, isOn: true, distance: 130, price: 2000"
+
+mustang.turnOff();
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000"
+
+console.log(Car.getSpecs(mustang));
+//"maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000"
+
+console.log(mustang.price);
+// 2000
+
+mustang.price = 4000;
+console.log(mustang.price);
+// 4000
